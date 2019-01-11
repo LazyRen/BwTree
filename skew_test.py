948dd4c32525f1e2b2781d833d636bb238a057df
DaeIn Lee <prangia0ren@gmail.com>
2019-01-11 16:58:07 +0900

    slight modification on script

--- skew_test.py
+++ skew_test.py
@@ -2,6 +2,7 @@
 
 from subprocess import Popen, PIPE
 import os
+import sys
 from random import shuffle
 from timeit import default_timer as timer
 from math import log, floor
@@ -13,26 +14,20 @@ EXECUTABLE_ARGV = '--new-skew-update'
 
 def test_case(total_thread_num):
     start = timer()
-    for i in range(1, total_thread_num + 1):
+    runPerf = False
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'perf':
+            print("Recording Perf Enabled")
+            runPerf = True
+    for i in range(0, total_thread_num + 1):
         # cmd = "./" + EXECUTABLE_NAME + (EXECUTABLE_ARGV % i)
-        print("RUNNING SKEW TEST WITH %d threads" % i)
+        print("RUNNING SKEW TEST with %d threads" % i)
         with Popen(["./" + EXECUTABLE_NAME, EXECUTABLE_ARGV, str(i)], cwd=os.path.dirname(os.path.realpath(__file__)), stdin=PIPE, stdout=PIPE, bufsize=1, universal_newlines=True) as p:
             for line in p.stdout:
                 print(line, end='') # process line here
-                if line.strip() == 'New Skew Update Test Starts':
-                    print("sudo perf record --call-graph dwarf -o skew%d.perf -p %d" % (i, p.pid))
+                if (line.strip() == 'New Skew Update Test Starts') and runPerf:
+                    print("pid %d perf will be recorded on skew%d.perf" % (p.pid, i))
                     os.system("sudo perf record --call-graph dwarf -o skew%d.perf -p %d" % (i, p.pid))
-        # p = Popen(["./" + EXECUTABLE_NAME, EXECUTABLE_ARGV, str(i)], cwd=os.path.dirname(os.path.realpath(__file__)), stdin=PIPE, stdout=PIPE, bufsize=1, universal_newlines=True)
-        # while True:
-        #     output = p.stdout.readline().decode('utf-8')
-        #     print(output)
-        #     if output == 'New Skew Update Test Starts':
-        #         break
-        # print("sudo perf record --call-graph dwarf -o skew%d.perf -p %d" % (i, p.pid))
-        # os.system("sudo perf record --call-graph dwarf -o skew%d.perf -p %d" % (i, p.pid))
-        # while True:
-        #     output = p.stdout.readline().decode('utf-8')
-        #     print(output)
     end = timer()
 
     return end - start
